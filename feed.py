e101d31f92e85970e9f57b975135e0388e1afd9b
wanghaisheng <edwin.uestc@gmail.com>
2022-05-28 15:49:59 +0800

    draft

---
+++ feed.py
@@ -0,0 +1,68 @@
+from feedgen.feed import FeedGenerator
+
+import json
+import yt_dlp
+import re
+import os
+# URL = 'https://youtube.com/channel/UCnDWguR8mE2oDBsjhQkgbvg'
+# URL = 'https://youtube.com/channel/UCBSQxFi6a8Ju2v_hgiM78Ew'
+URL = os.getenv('URL')
+channeid=URL.replace('https://youtube.com/channel/','')
+# channeid = 'UCBSQxFi6a8Ju2v_hgiM78Ew'
+yourowndomain = 'https://distbit.loophole.site/'+channeid+'/'
+
+
+def downloadvideosfromchannel(url, videodir):
+    # ℹ️ See help(yt_dlp.YoutubeDL) for a list of available options and public functions
+    ydl_opts = {
+        'outtmpl': videodir+'/%(id)s'+'.mp4',
+        'format': 'best',
+        # 'proxy': 'socks5://127.0.0.1:1080'
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(URL, download=False)
+        # with open('t.json', 'w', encoding='utf8') as f:
+            # f.write(json.dumps(ydl.sanitize_info(info)))
+
+        fg = FeedGenerator()
+        fg.load_extension('podcast')
+
+        fg.podcast.itunes_author = info['uploader']
+        fg.podcast.itunes_block = 'yes'
+        fg.podcast.itunes_image = info['thumbnail']
+        fg.podcast.itunes_explicit = 'no'
+        fg.podcast.itunes_complete = None
+        fg.podcast.itunes_new_feed_url = None
+        fg.podcast.itunes_owner = None
+        fg.podcast.itunes_subtitle = info['uploader']
+        fg.podcast.itunes_summary = None
+        fg.podcast.itunes_category(','.join(info['categories']))
+        fg.podcast.channel_title(info['channel'])
+        for idx,entry in enumerate(info['entries']):
+            fe = fg.add_entry()
+            fe.id(entry['id'])
+            fe.title(entry['title'])
+            fe.link(entry['webpage_url'])
+            fe.description(entry['description'])
+            fe.enclosure(yourowndomain+entry['id']+'.mp4', 0, 'video/mp4')
+
+            fe.itunes_author = entry['uploader']
+            fe.itunes_block = None
+            fe.itunes_image = entry['thumbnail']
+            fe.itunes_duration = entry['duration']
+            fe.itunes_explicit = 'no'
+        #     fe.itunes_is_closed_captioned = None
+            fe.itunes_order = str(idx)
+            fe.itunes_subtitle = entry['title']
+            fe.itunes_summary = '<![CDATA[{}]]'.format(entry['description'])
+            fe.itunes_season = None
+            fe.itunes_episode = None
+            fe.itunes_title = entry['fulltitle']
+            fe.itunes_episode_type = None
+
+        fg.rss_str(pretty=True)
+        fg.rss_file(channeid+'.xml')
+
+if not os.path.exists(channeid):
+    os.mkdir(channeid)
+downloadvideosfromchannel(URL, './'+channeid)
