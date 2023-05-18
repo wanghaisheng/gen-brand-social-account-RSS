a763565c33135f4b7890dd089eb57da290456075
HeisenBerg? <edwin.uestc@gmail.com>
2023-05-18 22:25:58 +0800

    Update feed.py

--- feed.py
+++ feed.py
@@ -24,51 +24,57 @@ def downloadvideosfromchannel(url, downloadVideo,videodir):
         'format': 'bestvideo[height<={}][ext=mp4][vcodec^=avc1]+bestaudio[ext=m4a]/best[height<={}][ext=mp4][vcodec^=avc1]/best[ext=mp4]/best'.format(480,480),
         # 'proxy': 'socks5://127.0.0.1:1080'
     }
+    fg = FeedGenerator()
+    fg.load_extension('podcast')    
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         if not downloadVideo:
             downloadVideo=True
-        info = ydl.extract_info(URL, download=downloadVideo)
+        try:
+            info = ydl.extract_info(URL, download=downloadVideo)
+
+            fg.title(info['channel'])
+            fg.link(href=info['uploader_url'])
+            fg.description(info['uploader'])
+            fg.podcast.itunes_author = info['uploader']
+            fg.podcast.itunes_block = 'yes'
+            fg.podcast.itunes_image = info['thumbnails'][0]['url']
+            fg.podcast.itunes_explicit = 'no'
+            fg.podcast.itunes_complete = None
+            fg.podcast.itunes_new_feed_url = None
+            fg.podcast.itunes_owner = None
+            fg.podcast.itunes_subtitle = info['uploader']
+            fg.podcast.itunes_summary = None
+            fg.podcast.itunes_category(','.join(info['entries'][0]['categories']))
+            # fg.podcast.channel_title(info['channel'])
+            for idx,entry in enumerate(info['entries']):
+                fe = fg.add_entry()
+                fe.id(entry['id'])
+                fe.title(entry['title'])
+                fe.link(href=entry['webpage_url'])
+                fe.description(entry['description'])
+                fe.enclosure(yourowndomain+entry['id']+'.mp4', 0, 'video/mp4')
+
+                fe.itunes_author = entry['uploader']
+                fe.itunes_block = None
+                fe.itunes_image = entry['thumbnail']
+                fe.itunes_duration = entry['duration']
+                fe.itunes_explicit = 'no'
+            #     fe.itunes_is_closed_captioned = None
+                fe.itunes_order = str(idx)
+                fe.itunes_subtitle = entry['title']
+                fe.itunes_summary = '<![CDATA[{}]]'.format(entry['description'])
+                fe.itunes_season = None
+                fe.itunes_episode = None
+                fe.itunes_title = entry['fulltitle']
+                fe.itunes_episode_type = None
+        except Exception as e:  # skipcq: PYL-W0703
+            log.debug(e)
+            print(
+                f"Failed to download {song.get('name')}, make sure yt_dlp is up to date"
+            )
+            continue
 #         with open(channeid+'.json', 'w', encoding='utf8') as f:
 #             f.write(json.dumps(ydl.sanitize_info(info)))
-
-        fg = FeedGenerator()
-        fg.load_extension('podcast')
-        fg.title(info['channel'])
-        fg.link(href=info['uploader_url'])
-        fg.description(info['uploader'])
-        fg.podcast.itunes_author = info['uploader']
-        fg.podcast.itunes_block = 'yes'
-        fg.podcast.itunes_image = info['thumbnails'][0]['url']
-        fg.podcast.itunes_explicit = 'no'
-        fg.podcast.itunes_complete = None
-        fg.podcast.itunes_new_feed_url = None
-        fg.podcast.itunes_owner = None
-        fg.podcast.itunes_subtitle = info['uploader']
-        fg.podcast.itunes_summary = None
-        fg.podcast.itunes_category(','.join(info['entries'][0]['categories']))
-        # fg.podcast.channel_title(info['channel'])
-        for idx,entry in enumerate(info['entries']):
-            fe = fg.add_entry()
-            fe.id(entry['id'])
-            fe.title(entry['title'])
-            fe.link(href=entry['webpage_url'])
-            fe.description(entry['description'])
-            fe.enclosure(yourowndomain+entry['id']+'.mp4', 0, 'video/mp4')
-
-            fe.itunes_author = entry['uploader']
-            fe.itunes_block = None
-            fe.itunes_image = entry['thumbnail']
-            fe.itunes_duration = entry['duration']
-            fe.itunes_explicit = 'no'
-        #     fe.itunes_is_closed_captioned = None
-            fe.itunes_order = str(idx)
-            fe.itunes_subtitle = entry['title']
-            fe.itunes_summary = '<![CDATA[{}]]'.format(entry['description'])
-            fe.itunes_season = None
-            fe.itunes_episode = None
-            fe.itunes_title = entry['fulltitle']
-            fe.itunes_episode_type = None
-
         fg.rss_str(pretty=True)
         fg.rss_file(channeid+'.xml')
 if not os.path.exists(channeid):
