b5d433c55d3d9f8299fcab2c7ac73a1da85586dc
HeisenBerg? <edwin.uestc@gmail.com>
2023-05-18 21:51:50 +0800

    Update feed.py

--- feed.py
+++ feed.py
@@ -30,47 +30,47 @@ def downloadvideosfromchannel(url, downloadVideo,videodir):
         info = ydl.extract_info(URL, download=downloadVideo)
 #         with open(channeid+'.json', 'w', encoding='utf8') as f:
 #             f.write(json.dumps(ydl.sanitize_info(info)))
+        if len(info['entries'])!==0:
+            fg = FeedGenerator()
+            fg.load_extension('podcast')
+            fg.title(info['channel'])
+            fg.link(href=info['uploader_url'])
+            fg.description(info['uploader'])
+            fg.podcast.itunes_author = info['uploader']
+            fg.podcast.itunes_block = 'yes'
+            fg.podcast.itunes_image = info['thumbnails'][0]['url']
+            fg.podcast.itunes_explicit = 'no'
+            fg.podcast.itunes_complete = None
+            fg.podcast.itunes_new_feed_url = None
+            fg.podcast.itunes_owner = None
+            fg.podcast.itunes_subtitle = info['uploader']
+            fg.podcast.itunes_summary = None
+            fg.podcast.itunes_category(','.join(info['entries'][0]['categories']))
+            # fg.podcast.channel_title(info['channel'])
+            for idx,entry in enumerate(info['entries']):
+                fe = fg.add_entry()
+                fe.id(entry['id'])
+                fe.title(entry['title'])
+                fe.link(href=entry['webpage_url'])
+                fe.description(entry['description'])
+                fe.enclosure(yourowndomain+entry['id']+'.mp4', 0, 'video/mp4')
 
-        fg = FeedGenerator()
-        fg.load_extension('podcast')
-        fg.title(info['channel'])
-        fg.link(href=info['uploader_url'])
-        fg.description(info['uploader'])
-        fg.podcast.itunes_author = info['uploader']
-        fg.podcast.itunes_block = 'yes'
-        fg.podcast.itunes_image = info['thumbnails'][0]['url']
-        fg.podcast.itunes_explicit = 'no'
-        fg.podcast.itunes_complete = None
-        fg.podcast.itunes_new_feed_url = None
-        fg.podcast.itunes_owner = None
-        fg.podcast.itunes_subtitle = info['uploader']
-        fg.podcast.itunes_summary = None
-        fg.podcast.itunes_category(','.join(info['entries'][0]['categories']))
-        # fg.podcast.channel_title(info['channel'])
-        for idx,entry in enumerate(info['entries']):
-            fe = fg.add_entry()
-            fe.id(entry['id'])
-            fe.title(entry['title'])
-            fe.link(href=entry['webpage_url'])
-            fe.description(entry['description'])
-            fe.enclosure(yourowndomain+entry['id']+'.mp4', 0, 'video/mp4')
+                fe.itunes_author = entry['uploader']
+                fe.itunes_block = None
+                fe.itunes_image = entry['thumbnail']
+                fe.itunes_duration = entry['duration']
+                fe.itunes_explicit = 'no'
+            #     fe.itunes_is_closed_captioned = None
+                fe.itunes_order = str(idx)
+                fe.itunes_subtitle = entry['title']
+                fe.itunes_summary = '<![CDATA[{}]]'.format(entry['description'])
+                fe.itunes_season = None
+                fe.itunes_episode = None
+                fe.itunes_title = entry['fulltitle']
+                fe.itunes_episode_type = None
 
-            fe.itunes_author = entry['uploader']
-            fe.itunes_block = None
-            fe.itunes_image = entry['thumbnail']
-            fe.itunes_duration = entry['duration']
-            fe.itunes_explicit = 'no'
-        #     fe.itunes_is_closed_captioned = None
-            fe.itunes_order = str(idx)
-            fe.itunes_subtitle = entry['title']
-            fe.itunes_summary = '<![CDATA[{}]]'.format(entry['description'])
-            fe.itunes_season = None
-            fe.itunes_episode = None
-            fe.itunes_title = entry['fulltitle']
-            fe.itunes_episode_type = None
-
-        fg.rss_str(pretty=True)
-        fg.rss_file(channeid+'.xml')
+            fg.rss_str(pretty=True)
+            fg.rss_file(channeid+'.xml')
 
 
 if not os.path.exists(channeid):
