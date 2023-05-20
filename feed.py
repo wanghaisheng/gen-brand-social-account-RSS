ce1213987eac8f0a9367076a957c940d7f01da61
HeisenBerg? <edwin.uestc@gmail.com>
2023-05-20 23:14:33 +0800

    Update feed.py

--- feed.py
+++ feed.py
@@ -12,7 +12,7 @@ downloadVideo = os.getenv('downloadVideo')
 print('whether download video:',downloadVideo)
 
 
-def youtubechannelrssfromurl(URL):
+def get_cid_from_URL(URL):
 
 
     # regular channel url  pattern 
@@ -48,9 +48,12 @@ def youtubechannelrssfromurl(URL):
                 cid=cid.replace('/','')       
             URL ="https://www.youtube.com/@"+cid            
         print("start processing---\n",URL)    
+        return cid
+    else:
+        return None
+def youtubechannelrssfromurl(URL):
 
-
-
+    try:
         # ℹ️ See help(yt_dlp.YoutubeDL) for a list of available options and public functions
         ydl_opts = {
             'verbose': True,
@@ -66,11 +69,73 @@ def youtubechannelrssfromurl(URL):
 
             except Exception as e:  # skipcq: PYL-W0703
                 print(e)
+
                 return None        
     else:
         print('invalid url')
         return None
-def downloadvideosfromchannel(url, downloadVideo,videodir,Height):
+def gen_Rss_from_url(URL):
+    # ℹ️ See help(yt_dlp.YoutubeDL) for a list of available options and public functions
+    print('your preferred is :',downloadVideo,Height)
+    ydl_opts = {
+        # 'proxy': 'socks5://127.0.0.1:1080'
+        'verbose': True,
+
+    }
+    fg = FeedGenerator()
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+
+        try:
+            info = ydl.extract_info(URL)
+            print(json.dumps(ydl.sanitize_info(info)))
+            with open(cid+'.json', 'w', encoding='utf8') as f:
+                f.write(json.dumps(ydl.sanitize_info(info)))
+            fg.load_extension('podcast')    
+                
+            fg.title(info['channel'])
+            fg.link(href=info['uploader_url'])
+            fg.description(info['uploader'])
+            fg.podcast.itunes_author = info['uploader']
+            fg.podcast.itunes_block = 'yes'
+            fg.podcast.itunes_image = info['thumbnails'][0]['url']
+            fg.podcast.itunes_explicit = 'no'
+            fg.podcast.itunes_complete = None
+            fg.podcast.itunes_new_feed_url = None
+            fg.podcast.itunes_owner = None
+            fg.podcast.itunes_subtitle = info['uploader']
+            fg.podcast.itunes_summary = None
+            fg.podcast.itunes_category(','.join(info['entries'][0]['categories']))
+            # fg.podcast.channel_title(info['channel'])
+            for idx,entry in enumerate(info['entries']):
+                fe = fg.add_entry()
+                fe.id(entry['id'])
+                fe.title(entry['title'])
+                fe.link(href=entry['webpage_url'])
+                fe.description(entry['description'])
+                fe.enclosure(yourowndomain+entry['id']+'.mp4', 0, 'video/mp4')
+
+                fe.itunes_author = entry['uploader']
+                fe.itunes_block = None
+                fe.itunes_image = entry['thumbnail']
+                fe.itunes_duration = entry['duration']
+                fe.itunes_explicit = 'no'
+            #     fe.itunes_is_closed_captioned = None
+                fe.itunes_order = str(idx)
+                fe.itunes_subtitle = entry['title']
+                fe.itunes_summary = '<![CDATA[{}]]'.format(entry['description'])
+                fe.itunes_season = None
+                fe.itunes_episode = None
+                fe.itunes_title = entry['fulltitle']
+                fe.itunes_episode_type = None
+            fg.rss_str(pretty=True)                
+        except Exception as e:  # skipcq: PYL-W0703
+            print(e)
+
+            fg.title('xxxx')
+            fg.link(href=URL)
+            fg.description('xxxx')
+        fg.rss_file(cid+'.xml')    
+def downloadvideosfromfreshchannel(URL, downloadVideo,videodir,Height):
     # ℹ️ See help(yt_dlp.YoutubeDL) for a list of available options and public functions
     print('your preferred is :',downloadVideo,Height)
     ydl_opts = {
@@ -134,6 +199,23 @@ def downloadvideosfromchannel(url, downloadVideo,videodir,Height):
             fg.description('xxxx')
         fg.rss_file(cid+'.xml')
         
+
+cid = get_cid_from_URL(URL)
+# after each video to podcast sync,we store local rss file to futher comparison
+# filename would be platform_cid.xml or platform_cid.json
+# if local rss file not exist we request a youtubechannelrssfromurl
+# if youtubechannelrssfromurl failed ,we gen_Rss_from_url
+gen_Rss_from_url(URL)        
+
+# if local rss exist,we get remote rss using cid ,
+# json =get(rssurl)
+
+## compare two files to find differences
+
+## download diff audio,thumbnail from video to later uploading 
+
+## publish to target platform
+
 # regular channel url  pattern 
 # URL = 'https://youtube.com/channel/UCnDWguR8mE2oDBsjhQkgbvg'
 # URL = 'https://youtube.com/channel/UCBSQxFi6a8Ju2v_hgiM78Ew'
@@ -173,7 +255,7 @@ if URL.startswith(('https://youtube.com/channel/', 'https://www.youtube.com/chan
         if not os.path.exists(cid):
             print('prepare dir:',cid)
             os.mkdir(cid)
-        downloadvideosfromchannel(URL,downloadVideo, './'+cid,Height)
+        downloadvideosfromfreshchannel(URL,downloadVideo, './'+cid,Height)
     else:
         print('we can move on to next step',rssURL)
         
