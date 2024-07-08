dc0f123145822551a01665c0df38c424f89622af
HeisenBerg? <edwin.uestc@gmail.com>
2024-07-09 07:41:43 +0800

    Update downloadVideos.py

--- downloadVideos.py
+++ downloadVideos.py
@@ -82,7 +82,7 @@ def get_cid_from_URL(URL):
 
 def downloadvideosfromfreshchannel(
     URL,
-    isDownloadVideo,
+    isDownloadVideo: bool=False,
     videodir,
     Height,
     isSubtitle: bool = False,
