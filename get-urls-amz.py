34ae6047c9eb37fd5f1911a6de82b038026c766f
HeisenBerg? <edwin.uestc@gmail.com>
2024-07-14 17:03:22 +0800

    Update get-urls-amz.py

--- get-urls-amz.py
+++ get-urls-amz.py
@@ -7,91 +7,95 @@ from aiohttp_socks import ProxyType, ProxyConnector
 import shutil
 import zipfile
 proxy_url = 'socks5://127.0.0.1:1080'  # 填写你的代理服务器地址
-proxy_url=None
 domain = 'https://www.amazon.com/sp?'  # 你的域名
 
-async def geturls(session, domain):
-    no_subs = None
+async def geturls(domain):
+    no_subs=None
     subs_wildcard = "*." if not no_subs else ""
-    domainname = domain.replace("https://", "").split('/')[0]
+    # subs_wildcard=''
+    domainname = domain.replace("https://", "")
+    domainname=domainname.split('/')[0]
     csv_file = f'./result/waybackmachines-{domainname}.csv'
 
-    headers = {
-        'Referer': 'https://web.archive.org/',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
-    }
-
-    query_url = f"http://web.archive.org/cdx/search/cdx?url={subs_wildcard}{domain}/*&fl=timestamp,original"
-    query_url=query_url+'&matchType=prefix'
+    query_url = f"http://web.archive.org/cdx/search/cdx?url={subs_wildcard}{domain}/&fl=timestamp,original,mimetype,statuscode,digest"
+    query_url = f"http://web.archive.org/cdx/search/cdx?url={domain}/&matchType=prefix&fl=timestamp,original"
+    fileter='&statuscode=200'
     filter="&collapse=urlkey"
     query_url=query_url+filter
+    query_url=query_url+'&matchType=prefix'
 
-    try:
+    # For example: http://web.archive.org/cdx/search/cdx?url=archive.org&from=2010&to=2011
 
-        async with session.get(query_url, headers=headers,
-                                #  proxy='http://127.0.0.1:1080', 
-                                timeout=30000) as resp:
-            if resp.status != 200:
-                print(f"Failed to get data, status: {resp.status}")
-                return
 
-            count = 0
-            file_exists = os.path.isfile(csv_file)
 
-            # 使用 iter_any 替代 iter_lines
-            # async for chunk in resp.content.iter_any():
-            async for chunk in resp.content.iter_chunked(10240):
+    # query_url='https://web.archive.org/__wb/sparkline?output=json&url=toolify.ai&collection=web'
+    # connector = ProxyConnector.from_url(proxy_url)
+    headers = {'Referer': 'https://web.archive.org/',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                                'Chrome/92.0.4515.107 Safari/537.36'}
+    # initialize an AIOHTTP client with the SOCKS proxy connector
+    async with aiohttp.ClientSession(connector=None) as session:
+        # Perform your web requests using the session
+        try:
+            resp=await session.get(query_url,
+                                headers=headers,
+                # auth=auth.prepare_request, 
+                # proxy='http://127.0.0.1:1080',
+                timeout=300000)     
+            count=0
+            while True:
+                raw = await resp.content.read(10240)  # 每次读取1024字节
+                if not raw:
+                    break
+                # raw=await resp.text()
+                # print(raw)
+                try:
 
-                if isinstance(chunk, str):
-                    lines = chunk.splitlines()
-                elif isinstance(chunk, bytes):
-                    lines = chunk.decode('utf-8', errors='ignore').splitlines()
-                else:
-                    continue
+                    raw = raw.decode('utf-8')
+                except UnicodeDecodeError:
+                    raw = raw.decode('latin-1')
+
+                if resp.status != 200:
+                # Handle different HTTP status codes
+                # return 
+                    print('not 200')
+            # Process the response
+                # print(raw)
+                lines = raw.splitlines()
+                fieldnames = ['date', 'url']
+
+                print(len(lines))
+                count=count+len(lines)
+                file_exists = os.path.isfile(csv_file)
 
                 for line in lines:
                     if ' ' in line:
-                        try:
-                            timestamp, original_url = line.strip().split(' ', 1)
-                            data = {'date': timestamp, 'url': original_url}
+                        # parts = line.split(' ')
+                        # timestamp, original_url = parts[0], parts[1]
+                        # data={'date':timestamp,'url':original_url}
+                        data={'url':line.strip()}
+                        with open(csv_file, mode='a', newline='', encoding='utf-8') as f:
+                            writer = csv.DictWriter(f, fieldnames=fieldnames)
+                            # if not file_exists:
+                                # writer.writeheader()
+                            writer.writerow(data)
+            print('============',count)
 
-                            with open(csv_file, mode='a', newline='', encoding='utf-8') as f:
-                                writer = csv.DictWriter(f, fieldnames=['date', 'url'])
-                                if not file_exists:
-                                    writer.writeheader()
-                                writer.writerow(data)
-                                file_exists = True
-                            count += 1
-                        except Exception as e:
-                            print(f"Error processing line: {e}")
+        except aiohttp.ClientError as e:
+            print(f"Connection error: {e}", 'red')
+            await geturls( domain)
 
-            print(f"Total URLs processed: {count}")
-
-    except aiohttp.ClientError as e:
-        print(f"Connection error: {e}")
-        # 可以在这里添加重试逻辑
-        # if 需要重试的条件:
-        await asyncio.sleep(5)
-        await geturls(session, domain)
-    except asyncio.TimeoutError:
-        print("The request timed out")
-        await asyncio.sleep(5)
-        await geturls(session, domain)
-
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        await asyncio.sleep(5)
-        await geturls(session, domain)
-
+        except Exception as e:
+            print(f"Couldn't get list of responses: {e}", 'red')
+            await geturls( domain)
+        # return out
 async def main(domain):
     retries = 5
     for i in range(retries):
         try:
-            connector = ProxyConnector.from_url(proxy_url) if proxy_url else None
 
-            async with aiohttp.ClientSession(connector=None) as session:
-                await geturls(session, domain)
-                break  # 如果成功，则退出循环
+            await geturls(domain)
+            break  # 如果成功，则退出循环
         except (aiohttp.ClientError, asyncio.TimeoutError) as e:
             print(f"Connection error on attempt {i+1}: {e}")
             if i < retries - 1:
