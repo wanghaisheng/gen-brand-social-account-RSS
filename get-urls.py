c48764f2b805a695842caf9b8a4e7d204ead2b16
HeisenBerg? <edwin.uestc@gmail.com>
2024-07-14 16:16:22 +0800

    Update get-urls.py

--- get-urls.py
+++ get-urls.py
@@ -7,23 +7,24 @@ from aiohttp_socks import ProxyType, ProxyConnector
 import shutil
 import zipfile
 proxy_url = 'socks5://127.0.0.1:1080'  # 填写你的代理服务器地址
+proxy_url=None
 domain = 'https://www.amazon.com/sp?'  # 你的域名
 
 async def geturls(session, domain):
     no_subs = None
     subs_wildcard = "*." if not no_subs else ""
     domainname = domain.replace("https://", "").split('/')[0]
-    csv_file =f'./result/waybackmachines-{domainname}.csv'
+    csv_file = f'./result/waybackmachines-{domainname}.csv'
 
     headers = {
         'Referer': 'https://web.archive.org/',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
     }
 
-    query_url = f"http://web.archive.org/cdx/search/cdx?url={domain}/*&fl=timestamp,original"
+    query_url = f"http://web.archive.org/cdx/search/cdx?url={subs_wildcard}{domain}/*&fl=timestamp,original"
+    query_url=query_url+'&matchType=prefix'
     filter="&collapse=urlkey"
     query_url=query_url+filter
-    # query_url=query_url+'&limit=10'
 
     try:
 
@@ -39,7 +40,7 @@ async def geturls(session, domain):
 
             # 使用 iter_any 替代 iter_lines
             # async for chunk in resp.content.iter_any():
-            async for chunk in resp.content.iter_chunked(1024):
+            async for chunk in resp.content.iter_chunked(10240):
 
                 if isinstance(chunk, str):
                     lines = chunk.splitlines()
@@ -70,8 +71,8 @@ async def geturls(session, domain):
         print(f"Connection error: {e}")
         # 可以在这里添加重试逻辑
         # if 需要重试的条件:
-        #     await asyncio.sleep(重试等待时间)
-        #     await geturls(session, domain)
+        await asyncio.sleep(5)
+        await geturls(session, domain)
     except asyncio.TimeoutError:
         print("The request timed out")
     except Exception as e:
@@ -161,28 +162,15 @@ if __name__ == "__main__":
     start_time = time.time()
     asyncio.run(main(domain))
     print(f"Time taken for asynchronous execution: {time.time() - start_time} seconds")
-    import os
-
-# 假设 domainname 变量已经被赋值
-    domainname = domain.replace("https://", "").split('/')[0]
-
-# 构建 CSV 文件的完整路径
-    csv_file_path = f'./result/waybackmachines-{domainname}.csv'
-
-# 检查 CSV 文件是否存在
-    if os.path.exists(csv_file_path):
-        print(f"The file '{csv_file_path}' exists.")
     # Specify the maximum size of each RAR file in MB
-        max_size_mb = 1500
+    max_size_mb = 1500
 
     # Create a temporary ZIP file for the first archive
-        zip_count = 1
-        zip_temp_file = os.path.join(folder_path, f"temp{zip_count}.zip")
-        zip_file = zipfile.ZipFile(zip_temp_file, "w", zipfile.ZIP_DEFLATED)
+    zip_count = 1
+    zip_temp_file = os.path.join(folder_path, f"temp{zip_count}.zip")
+    zip_file = zipfile.ZipFile(zip_temp_file, "w", zipfile.ZIP_DEFLATED)
 
     # Compress the folder into multiple ZIP archives
-        zip_folder(
+    zip_folder(
         folder_path, output_folder, max_size_mb, zip_file, zip_temp_file, zip_count
     )
-    else:
-        print(f"The file '{csv_file_path}' does not exist.")
